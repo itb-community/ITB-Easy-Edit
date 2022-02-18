1a1872031974f62ee4bd3d0b5153a8d938b21933
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-02-18 15:29:19 +0100

    Add onEditorButtonSet event

--- modules/events.lua
+++ modules/events.lua
@@ -2,3 +2,6 @@
 modApi.events.onIslandLeft = Event()
 modApi.events.onGameStateChanged = Event()
 modApi.events.onTilesetChanged = Event()
+
+easyEdit.events = {}
+easyEdit.events.onEditorButtonSet = Event()
