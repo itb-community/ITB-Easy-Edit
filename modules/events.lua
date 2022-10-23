e81569b06c6da8d544db537ff6a844fa93395f41
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-10-23 20:02:08 +0200

    Remove gameState.lua and currentTileset.lua
    Replace with mod loader events

--- modules/events.lua
+++ modules/events.lua
@@ -1,6 +1,3 @@
 
 easyEdit.events = {}
 easyEdit.events.onEditorButtonSet = Event()
-easyEdit.events.onIslandLeft = Event()
-easyEdit.events.onGameStateChanged = Event()
-easyEdit.events.onTilesetChanged = Event()
