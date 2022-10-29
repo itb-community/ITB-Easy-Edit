a66d30dced315a82430274af85ac65415b0becc5
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-10-29 23:06:53 +0200

    Remove references to enemy and squad editors

--- ui/menues.lua
+++ ui/menues.lua
@@ -1,7 +1,5 @@
 
 local path = GetParentPath(...)
-local enemyEditor = require(path.."editor_enemy")
-local squadEditor = require(path.."editor_squad")
 local enemyListEditor = require(path.."editor_enemyList")
 local bossListEditor = require(path.."editor_bossList")
 local missionListEditor = require(path.."editor_missionList")
@@ -9,12 +7,6 @@ local structureListEditor = require(path.."editor_structureList")
 local islandEditor = require(path.."editor_island")
 local worldEditor = require(path.."editor_world")
 
-local MECH_TITLE = "Mech Editor"
-local MECH_TOOLTIP = "Create new mechs, or edit existing ones"
-local ENEMY_TITLE = "Enemy Editor"
-local ENEMY_TOOLTIP = "Create new enemies, or edit existing ones"
-local SQUAD_TITLE = "Squad Editor"
-local SQUAD_TOOLTIP = "Create new squads, or edit existing ones"
 local ENEMY_LIST_TITLE = "Enemy Lists Editor"
 local ENEMY_LIST_TOOLTIP = "Create new enemy lists, or edit existing ones"
 local BOSS_LIST_TITLE = "Boss Lists Editor"
@@ -28,9 +20,6 @@ local ISLAND_TOOLTIP = "Create new islands, or edit existing ones"
 local WORLD_TITLE = "World Editor"
 local WORLD_TOOLTIP = "Arrange the content of the 4 playable islands"
 
--- sdlext.addModContent(MECH_TITLE, mechEditor.mainButton, MECH_TOOLTIP)
--- sdlext.addModContent(ENEMY_TITLE, enemyEditor.mainButton, ENEMY_TOOLTIP)
--- sdlext.addModContent(SQUAD_TITLE, squadEditor.mainButton, SQUAD_TOOLTIP)
 sdlext.addModContent(ENEMY_LIST_TITLE, enemyListEditor.mainButton, ENEMY_LIST_TOOLTIP)
 sdlext.addModContent(BOSS_LIST_TITLE, bossListEditor.mainButton, BOSS_LIST_TOOLTIP)
 sdlext.addModContent(MISSION_LIST_TITLE, missionListEditor.mainButton, MISSION_LIST_TOOLTIP)
