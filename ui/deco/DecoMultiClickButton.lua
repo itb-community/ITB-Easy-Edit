996e0046d70c7019713e5dc927c299057d1f1dc8
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2023-04-20 00:22:41 +0200

    Add disabled state for DecoMultiClickButton
    Also update helper_surface with additional surfaces with disabled colors.
    Also update helper_resetButton with additional argument specifying disabled surface.

--- ui/deco/DecoMultiClickButton.lua
+++ ui/deco/DecoMultiClickButton.lua
@@ -1,17 +1,20 @@
 
 local DecoMultiClickButton = Class.inherit(DecoSurfaceAligned)
 
-function DecoMultiClickButton:new(surfaces, surfaceshl, alignH, alignV)
+function DecoMultiClickButton:new(surfaces, surfaceshl, surfacedisabled, alignH, alignV)
 	DecoSurfaceAligned.new(self, surfaces[1], alignH, alignV)
 
 	self.surfaces = shallow_copy(surfaces)
 	self.surfaceshl = shallow_copy(surfaceshl) or self.surfaces
+	self.surfacedisabled = surfacedisabled or self.surfaces[1]
 end
 
 function DecoMultiClickButton:draw(screen, widget)
 	local index = (widget.clickCount or 0) + 1
 
-	if widget.hovered then
+	if widget.disabled then
+		self.surface = self.surfacedisabled
+	elseif widget.hovered then
 		self.surface = self.surfaceshl[index]
 	else
 		self.surface = self.surfaces[index]
