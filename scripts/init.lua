c7e19a3f5ac53635a8179da23fb0b857cfa0361e
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2025-03-26 21:30:09 -0700

    Update to 2.0.8

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,8 +2,8 @@
 local extension = {
 	id = "easyEdit",
 	name = "Easy Edit",
-	version = "2.0.7",
-	modApiVersion = "2.9.3",
+	version = "2.0.8",
+	modApiVersion = "2.9.4",
 	gameVersion = "1.2.88",
 	icon = "img/icon.png",
 	isExtension = true,
